c4938557524cc8daa896c74ff500a44160617ab6
zxc22665513 <55220432+zxc22665513@users.noreply.github.com>
2019-10-15 21:01:06 +0800

    Create 2_網路攻擊模式分析_temp.sol

---
+++ IPAS/2_網路攻擊模式分析_temp.sol
@@ -0,0 +1,125 @@
+#
+```
+
+```
+```
+#1. John is analyzing(分析) strange(奇怪) behavior on computers in his network. He believes there is malware
+on the machines. The symptoms include strange behavior that persists, even if he
+boots the machine to a Linux Live CD. What is the most likely cause?
+A. Ransomware(勒索病毒)
+B. Boot(啟動) sector(區域) virus
+C. Rootkit
+D. Key logger (鍵盤監聽工具)
+Ans:B
+‵‵
+#2. Ahmed is a sales manager with a major insurance company. He has received an email that
+is encouraging him to click on a link and fill out a survey. He is suspicious(懷疑) of the email,
+but it does mention a major insurance association, and that makes him think it might be
+legitimate(合法). 
+
+Which of the following best describes this attack?
+A. Phishing (網路釣魚)
+B. Social engineering (社交工程)
+C. Spear phishing (魚叉式網路釣魚)(Spear Phishing)特定對象
+D. Trojan horse (特洛伊木馬)
+```
+```
+Which of the following 
+Ans:C
+```
+```
+3. You are a security administrator for a medium-sized bank. You have discovered a piece of
+software on your bank’s database server that is not supposed to be there. It appears that
+the software will begin deleting database files if a specific employee is terminated. What
+best describes this?
+A. Worm
+B. Logic bomb
+C. Trojan horse
+D. Rootkit
+```
+```
+4. You are responsible for incident response at Acme bank. The Acme bank website has been
+attacked. The attacker used the login screen, but rather than enter login credentials, he or
+she entered some odd text: ' or '1' = '1. What is the best description for this attack?
+A. Cross-site scripting
+B. Cross-site request forgery
+C. SQL injection
+D. ARP poisoning
+```
+```
+5. Juanita is a network administrator for a small accounting firm. The users on her network
+are complaining (抱怨) of slow connectivity(很慢的連線). When she examines the firewall logs, she observes a
+large number of half-open connections. What best describes this attack?
+A. DDoS
+B. SYN flood
+C. Buffer overflow
+D. ARP poisoning
+```
+a large number of half-open connections==(一堆半開放式的連線)
+Ans:B
+```
+```
+6. Frank is deeply concerned about attacks to his company’s e-commerce server. He is particularly
+worried about cross-site scripting and SQL injection. Which of the following
+would best defend against these two specific attacks?
+A. Encrypted web traffic
+B. Filtering user input
+C. A firewall
+D. An IDS
+```
+```
+#10. Your company outsourced(外包) development of an accounting application to a local programming
+firm. After three months of using the product, one of your accountants(會計師) accidently
+discovers a way to log in(登入) and bypass(繞過) all security and authentication(認證).
+What best describesthis?
+A. Logic bomb (邏輯炸彈)
+B. Trojan horse
+C. Backdoor (後門程式)
+D. Rootkit
+```
+```
+116. Which of the following would a former employee most likely plant on a server before leaving
+to cause disruption to the network?
+A. Worm
+B. Logic bomb
+C. Trojan
+D. Virus
+```
+```
+117. A SYN flood is a DoS attack in which an attacker deliberately violates the three-way
+handshake and opens a large number of half-open TCP connections. The signature of a
+SYN flood attack is:
+A. The source and destination address having the same value
+B. The source and destination port numbers having the same value
+C. A large number of SYN packets appearing on a network without the corresponding
+ACK packets
+D. A large number of SYN packets appearing on a network with the corresponding
+reply RST
+```
+```
+118. What does white-box testing mean?
+A. The tester has full knowledge of the environment.
+B. The tester has no knowledge of the environment.
+C. The tester has permission to access the system.
+D. The tester has no permission to access the system.
+```
+```
+119. Ahmed has been hired to perform a penetration test of Acme Corporation. He begins by
+looking at IP address ranges owned by the company and details of domain name registration.
+He also visits social media and newsgroups to see if they contain any sensitive information
+or have any technical details online. Within the context of penetration-examining
+methodology, what phase is Ahmed conducting?
+A. Passive information gathering
+B. Active information gathering
+C. Initial exploitation
+D. Vulnerability scanning
+```
+```
+120. Mary works for a large insurance company, on their cybersecurity team. She is investigating
+a recent incident and discovers that a server was breached using an authorized user’s
+account. After investigating the incident further, Mary believes that the authorized user
+logged on, and then someone else took over their session. What best describes this attack?
+A. Man-in-the-middle
+B. Session hijacking
+C. Backdoor
+D. Smurf attack
