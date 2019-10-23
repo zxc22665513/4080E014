f089a3d8a083d10d4564b73d39487ced0be8c73a
zxc22665513 <55220432+zxc22665513@users.noreply.github.com>
2019-10-23 20:57:48 +0800

    Update 2_網路攻擊模式分析_temp.sol

--- IPAS/2_網路攻擊模式分析_temp.sol
+++ IPAS/2_網路攻擊模式分析_temp.sol
@@ -86,24 +86,25 @@ A. Worm(電腦蠕蟲)(幾乎都對網路有害)
 B. Logic bomb(邏輯炸彈)
 C. Trojan(木馬)
 D. Virus(病毒)
+(A)
 ```
 ```
-117. A SYN flood is a DoS attack in which an attacker deliberately violates the three-way
-handshake and opens a large number of half-open TCP connections. The signature of a
-SYN flood attack is:
-A. The source and destination address having the same value
-B. The source and destination port numbers having the same value
+117. A SYN flood is a DoS attack(SYN泛洪是一種DoS攻擊) in which an attacker deliberately violates the three-way(攻擊者故意違反三者)
+handshake and opens a large number of half-open TCP connections.(並打開大量的半開式TCP連接) The signature of a
+SYN flood attack is:(SYN Flood攻擊的特徵是)
+A. The source and destination address having the same value(源地址和目的地址的值相同)
+B. The source and destination port numbers having the same value(源端口號和目標端口號具有相同的值)
 C. A large number of SYN packets appearing on a network without the corresponding
-ACK packets
+ACK packets(沒有相應的ACK數據包的大量SYN數據包出現在網絡上)
 D. A large number of SYN packets appearing on a network with the corresponding
-reply RST
+reply RST(網絡上出現大量帶有相應RST響應的SYN數據包)
 ```
 ```
-118. What does white-box testing mean?
-A. The tester has full knowledge of the environment.
-B. The tester has no knowledge of the environment.
-C. The tester has permission to access the system.
-D. The tester has no permission to access the system.
+118. What does white-box testing mean?(白盒測試是什麼意思？)
+A. The tester has full knowledge of the environment.(測試人員對環境有充分的了解。
+B. The tester has no knowledge of the environment.(測試人員不了解環境。
+C. The tester has permission to access the system.(測試人員有權訪問系統。
+D. The tester has no permission to access the system.(測試人員無權訪問系統。
 ```
 ```
 119. Ahmed has been hired to perform a penetration test of Acme Corporation. He begins by
@@ -111,17 +112,24 @@ looking at IP address ranges owned by the company and details of domain name reg
 He also visits social media and newsgroups to see if they contain any sensitive information
 or have any technical details online. Within the context of penetration-examining
 methodology, what phase is Ahmed conducting?
-A. Passive information gathering
-B. Active information gathering
-C. Initial exploitation
-D. Vulnerability scanning
+Ahmed已受聘執行Acme Corporation的滲透測試。 他首先查看了公司擁有的IP地址範圍和域名註冊的詳細信息
+，還訪問了社交媒體和新聞組以查看它們是否包含任何敏感信息或在線具有任何技術細節。
+在滲透檢查方法論的背景下，艾哈邁德進行哪個階段？
+A. Passive information gathering(被動信息收集
+B. Active information gathering(主動信息收集
+C. Initial exploitation(最初的剝削
+D. Vulnerability scanning(漏洞掃描
 ```
 ```
 120. Mary works for a large insurance company, on their cybersecurity team. She is investigating
 a recent incident and discovers that a server was breached using an authorized user’s
 account. After investigating the incident further, Mary believes that the authorized user
 logged on, and then someone else took over their session. What best describes this attack?
-A. Man-in-the-middle
-B. Session hijacking
-C. Backdoor
-D. Smurf attack
+瑪麗在一家大型保險公司的網絡安全團隊中工作。 她正在調查
+最近發生的事件，發現使用授權用戶的服務器違反了服務器
+帳戶。 在進一步調查事件之後，瑪麗認為授權用戶
+登錄，然後其他人接管了他們的會話。 什麼最能描述這種攻擊？
+A. Man-in-the-middle(中間人
+B. Session hijacking(連線劫持
+C. Backdoor(後門病毒
+D. Smurf attack(藍精靈攻擊
